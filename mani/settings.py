8a83bcef68c0e2a0e0d51835d8b83b17fc564f89
kartikverma09 <124339566+kartikverma09@users.noreply.github.com>
2023-08-24 10:45:52 +0530

    Update settings.py

--- mani/settings.py
+++ mani/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 
 MEDIA_URL = '/media/'
@@ -131,6 +131,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR/'static'
+# ]
